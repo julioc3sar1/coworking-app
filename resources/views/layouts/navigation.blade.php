7b00bcbe1cae8fef367e329781292595831abc4c
Julio Cesar <julioc3sarr1@gmail.com>
2024-11-12 16:22:23 +0100

    Add rooms navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('rooms')" :active="request()->routeIs('rooms')">
+                        {{ __('Salas') }}
+                    </x-nav-link>
                 </div>
             </div>
 
