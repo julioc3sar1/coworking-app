adab6aa068f7795644aa60b7e78feff0011491ae
Julio Cesar <julioc3sarr1@gmail.com>
2024-11-12 13:40:27 +0100

    Assign client role when user registers

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Spatie\Permission\Models\Role;
 
 class RegisteredUserController extends Controller
 {
@@ -40,6 +41,9 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $clienteRole = Role::where('name', 'cliente')->first();
+        $user->assignRole($clienteRole);
 
         event(new Registered($user));
 
