272cd7d49322c1f3737b46742a3a8a7733615c50
Julio Cesar <julioc3sarr1@gmail.com>
2024-11-12 18:44:26 +0100

    Add search field and create room button

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -7,13 +7,31 @@
 
     <div class="container">
         <div class="row">
+            <div class="col-12 mb-4">
+                <div class="row g-3 align-items-center">
+                    <div class="col-auto">
+                      <label for="inputPassword6" class="col-form-label">Buscar sala</label>
+                    </div>
+                    <div class="col-auto">
+                      <input type="text" id="inputSearch" class="form-control" aria-describedby="searchField">
+                    </div>
+                    <div class="col-auto ms-auto">
+                        <button class="btn">Nueva sala</button>
+                    </div>
+                </div>
+            </div>
             @foreach ($rooms as $room)
             <div class="col-sm-6 mb-3">
               <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">{{$room->name}}</h5>
                   <p class="card-text">{{$room->description}}</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                    @role('admin')
+                    <a href="#" class="btn btn-warning">Editar</a>
+                    <a href="#" class="btn btn-light">Eliminar</a>
+                    @else
+                    <a href="#" class="btn btn-primary">Reservar</a>
+                    @endrole
                 </div>
               </div>
             </div>
