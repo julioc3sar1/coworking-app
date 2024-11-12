057900aa0255351da5543e9e0ffa5864b337b038
Julio Cesar <julioc3sarr1@gmail.com>
2024-11-12 14:31:55 +0100

    Fix sala user table migration

--- database/migrations/2024_11_12_133129_create_sala_user_table.php
+++ database/migrations/2024_11_12_133129_create_sala_user_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('sala_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('sala_id')->constrained('salas')->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
