b1060491019e95fc07fef262c22b6c403a924662
Julio Cesar <julioc3sarr1@gmail.com>
2024-11-12 14:06:41 +0100

    Add user seeder
    this allows to create admin user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin'),
+        ]);
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $user->assignRole($adminRole);
+    }
+}
