cb559908987a6c889546a156a5f87fec726c1691
Julio Cesar <julioc3sarr1@gmail.com>
2024-11-13 10:10:45 +0100

    Use has factory booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Booking extends Model
 {
+    use HasFactory;
     protected $fillable = ['user_id', 'room_id', 'start_date', 'end_date'];
-    
+
     public function room(){
         return $this->belongsTo(Room::class);
     }
