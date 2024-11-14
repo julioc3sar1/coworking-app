cb02fa5cfad3670e03746c728d9c53e8f5eeae85
Julio Cesar <julioc3sarr1@gmail.com>
2024-11-14 10:41:44 +0100

    Remove margin from headers

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-0">
             {{ __('Reservas') }}
         </h2>
     </x-slot>
