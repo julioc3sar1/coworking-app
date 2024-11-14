61e00415787afd2d5abebcc6a311267eb8e592f9
Julio Cesar <julioc3sarr1@gmail.com>
2024-11-14 12:14:18 +0100

    Fix booking end date

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -8,6 +8,7 @@ use App\Models\Booking;
 use App\Models\Room;
 use Illuminate\Support\Facades\Auth;
 use App\Rules\Available;
+use Illuminate\Support\Carbon;
 
 class BookingController extends Controller
 {
@@ -34,14 +35,14 @@ class BookingController extends Controller
     public function store(Request $request){
         $request->merge([
             'start_date' => convertirFechaIsoAFormatoBD($request->start_date),
-            'end_date' => convertirFechaIsoAFormatoBD($request->start_date),
+            'end_date' => Carbon::parse(convertirFechaIsoAFormatoBD($request->start_date))->addHour(),
         ]);
 
         $validatedData = $request->validate([
             'room_id' => ['required','string','max:255',new Available],
             'user_id'=>'required|string|max:255',   
             'start_date'=>'required|date',
-            'end_date'=>'required|date|after_or_equal:start_date'
+            'end_date'=>'required|date|after:start_date'
         ]);
 
         Booking::create($validatedData);
